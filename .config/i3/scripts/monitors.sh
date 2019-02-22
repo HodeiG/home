de5332ba34de7e6e9ea1aac4f76b7e9759a6c46a
Hodei Goncalves <hodei.goncalves@gmail.com>
2019-02-22 18:35:04 +0000

    Changes in the monitors.sh script to set up secondary monitors.

--- .config/i3/scripts/monitors.sh
+++ .config/i3/scripts/monitors.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
-# https://faq.i3wm.org/question/5312/how-to-toggle-onoff-external-and-internal-monitors.1.html
-# https://wiki.archlinux.org/index.php/xrandr
-connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-activeOutput=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+# 
+# Script to enable/disable multiple monitors.
+# If multiple monitors are enabled, it will enable just the main monitor.
+# If only the main monitor is enabled enable all of them.
+# See also:
+# https://faq.i3wm.org/question/5312/how-to-toggle-onoff-external-and-internal-monitors.1.html 
+# https://wiki.archlinux.org/index.php/xrandr 
 
-if [ "$connectedOutputs" == "$activeOutput" ] ; then
+
+connected_outputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+active_output=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+secondary_monitor_output=
+
+if [ "$connected_outputs" == "$active_output" ] ; then
     # Disable multiple monitors
-    xrandr -o normal
+    secondary_monitor_output="--off"
 else
-    # Disable multiple monitors
-    xrandr -o normal
     # Enable multiple monitors
-    previous=""
-    execute="xrandr "
-    for display in $connectedOutputs ; do
-        execute="$execute --output $display --auto"
-        if [ "$previous" != "" ] ; then
+    secondary_monitor_output="--auto"
+fi
+# Disable multiple monitors
+# xrandr -o normal
+# Enable multiple monitors
+previous=""
+execute="xrandr "
+default_output="--auto"
+for display in $connected_outputs ; do
+    execute="$execute --output $display $default_output"
+    if [ "$previous" != "" ] ; then
+        if [ "$secondary_monitor_output" != "--off" ] ; then
             execute="$execute --right-of $previous"
         fi
-        previous="$display"
-    done
-    eval "$execute"
-fi
+    fi
+    default_output=$secondary_monitor_output
+    previous="$display"
+done
+echo $execute
+eval "$execute"
