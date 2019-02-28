500c8011826e99a4e9ca4a36b83dde9321961e55
Hodei Goncalves <hodei.goncalves@gmail.com>
2019-02-28 19:29:54 +0000

    Added changes in i3 config to configure monitors and change app bar colour.

--- .config/i3/scripts/monitors.sh
+++ .config/i3/scripts/monitors.sh
@@ -10,14 +10,40 @@
 
 connected_outputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 active_output=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-secondary_monitor_output=
 
-if [ "$connected_outputs" == "$active_output" ] ; then
-    # Disable multiple monitors
-    secondary_monitor_output="--off"
+monitor_mode=$1
+secondary_monitor_output=
+position=
+
+if [ -z "$monitor_mode" ] ; then
+    # If monitor mode not provided, switch between MAIN and RIGHT
+    if [ "$connected_outputs" == "$active_output" ] ; then
+        # Disable multiple monitors
+        secondary_monitor_output="--off"
+        position=""
+    else
+        # Enable multiple monitors
+        secondary_monitor_output="--auto"
+        position="--right-of"
+    fi
 else
-    # Enable multiple monitors
-    secondary_monitor_output="--auto"
+    if [ "$monitor_mode" == "MAIN" ] ; then
+        # Enable only main monitor
+        secondary_monitor_output="--off"
+        position=""
+    elif [ "$monitor_mode" == "RIGHT" ] ; then
+        # Enable multiple monitors set up on the right
+        secondary_monitor_output="--auto"
+        position="--right-of"
+    elif [ "$monitor_mode" == "LEFT" ] ; then
+        # Enable multiple monitors set up on the left
+        secondary_monitor_output="--auto"
+        position="--left-of"
+    elif [ "$monitor_mode" == "CLONE" ] ; then
+        # Enable multiple monitors cloned
+        secondary_monitor_output="--auto"
+        position="--same-as"
+    fi
 fi
 # Disable multiple monitors
 # xrandr -o normal
@@ -29,11 +55,11 @@ for display in $connected_outputs ; do
     execute="$execute --output $display $default_output"
     if [ "$previous" != "" ] ; then
         if [ "$secondary_monitor_output" != "--off" ] ; then
-            execute="$execute --right-of $previous"
+            execute="$execute $position $previous"
         fi
     fi
     default_output=$secondary_monitor_output
     previous="$display"
 done
-echo $execute
+echo "$execute"
 eval "$execute"
