2a3105a1e989e2ad25a26b64df52790e402917ef
Hodei Goncalves <hodei.goncalves@hpe.com>
2020-04-25 16:43:26 +0100

    Changed git-add-script to create local repo if remote exists or to create remote if local exists.

--- scripts/git-add-crypt.sh
+++ scripts/git-add-crypt.sh
@@ -5,49 +5,60 @@ function error() {
 }
 
 CRYPT_REPO=$HOME/Dropbox/workspace
-SRC_REPO=$HOME/src
-BACKEND_EXISTS=false
+LOCAL_EXISTED=false
+REMOTE_EXISTED=false
 
-test -n "$1" || error "Repository name not given."
+test -n "$1" || error "Full path to the repository. If it doesn't exist will \
+be created"
 test -n "$2" || error "GPG key not provided. See 'gpg --list-keys'"
-test ! -d "$SRC_REPO/$1" || error "Source repository already exists."
 
-REPO_NAME="$1"
+REPO_DIR=$(dirname "$1")
+REPO_NAME=$(basename "$1")
 GPG_KEY="$2"
 
 # set -x
 # set -v
 # set -e
-# 1. Create backend encrypted repo
+# 1. Create remote encrypted repo
 #==================================
-if [ ! -d "$CRYPT_REPO/$1" ] ; then
+if [ ! -d "$CRYPT_REPO/$REPO_NAME" ] ; then
     mkdir -p "$CRYPT_REPO/$REPO_NAME/src"
     mkdir -p "$CRYPT_REPO/$REPO_NAME/docs"
     cd "$CRYPT_REPO/$REPO_NAME/src" || error "Cannot cd to encrypted src directory."
     git init .
     git config --bool core.bare true
 else
-    echo "Backend encrypted repo already exists."
-    BACKEND_EXISTS=true
+    echo "Remote encrypted repo already exists."
+    REMOTE_EXISTED=true
 fi
 
-# 2. Create fronted source repo
+# 2. Create local source repo
 #===============================
-mkdir -p "$SRC_REPO/$REPO_NAME"
-cd "$SRC_REPO/$REPO_NAME" || error "Cannot cd to src directory."
-git init .
+mkdir -p "$REPO_DIR/$REPO_NAME"
+cd "$REPO_DIR/$REPO_NAME" || error "Cannot cd to src directory."
 
-git remote add crypt gcrypt::"$CRYPT_REPO/$REPO_NAME/src/"
-git config --add gcrypt.gpg-args "--use-agent"
-git config --add gcrypt.publish-participants true
-git config user.signingkey "$GPG_KEY"
-git config --add gcrypt.participants "$GPG_KEY"
-
-if $BACKEND_EXISTS ; then
-    git pull crypt master
+# If not git repo initialize it
+if ! git rev-parse --git-dir &> /dev/null ; then
+    git init .
 else
-    # 3. First commit
-    #===============================
+    echo "Local repo already exists."
+    LOCAL_EXISTED=true
+fi
+
+# If remote gcrypt doesn't exist add it
+if ! git remote show | grep -q crypt ; then
+    git remote add crypt gcrypt::"$CRYPT_REPO/$REPO_NAME/src/"
+    git config --add gcrypt.gpg-args "--use-agent"
+    git config --add gcrypt.publish-participants true
+    git config user.signingkey "$GPG_KEY"
+    git config --add gcrypt.participants "$GPG_KEY"
+fi
+
+# 3. Update local repo
+#===============================
+if $REMOTE_EXISTED ; then
+    git pull crypt master
+elif ! $LOCAL_EXISTED ; then
     touch README
     git add README
     git commit -a -m "First commit."
